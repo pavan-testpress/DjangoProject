2ba3699a98a20ad640e3c9bcae7e06889460e8c5
Pavan Kumar <Pavan@testpress.in>
2018-12-28 12:47:31 +0530

    Change Bookmarks, Folder models
    - Remove Unique Constraint on names & urls fileds on both models
    - Remove Auto generated comments
    - Change field name user to created_by(verbose)

--- DjangoProject/bookmarksapp/admin.py
+++ DjangoProject/bookmarksapp/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 
 from .models import Bookmarks, Folders
 
-# Register your models here.
-
 admin.site.register(Bookmarks)
 admin.site.register(Folders)
--- DjangoProject/bookmarksapp/models.py
+++ DjangoProject/bookmarksapp/models.py
@@ -5,13 +5,11 @@ from model_utils.models import TimeStampedModel
 
 from authenticationapp.models import MyUser
 
-# Create your models here.
-
 
 class Folders(TimeStampedModel):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(unique=True)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    created_by = models.ForeignKey(MyUser, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -20,10 +18,10 @@ class Folders(TimeStampedModel):
 
 class Bookmarks(TimeStampedModel):
     folder = models.ForeignKey(Folders, on_delete=models.SET_NULL, related_name='folder', null=True)
-    name = models.CharField(max_length=50, unique=True)
-    url = models.URLField(max_length=200, unique=True)
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, default=1)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=50)
+    url = models.URLField(max_length=200)
+    created_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    slug = models.SlugField()
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
