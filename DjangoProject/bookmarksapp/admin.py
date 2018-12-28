4c7db587460d75d79cf3029db79c4314930e3fe6
Pavan Kumar <Pavan@testpress.in>
2018-12-28 11:47:04 +0530

    Add Models to Bookmarks app
    - Add Bookmarksapp to Installed apps
    - Add Bookmarks Model
    - Add Folders Model
    - Add django_utils Pipfile
    - Migrate the Models
    - Remove Unused imports to follow flake8 guidelines
    - Add Bookmarks, Folders Models to Admin site

--- DjangoProject/bookmarksapp/admin.py
+++ DjangoProject/bookmarksapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Bookmarks, Folders
+
 # Register your models here.
+
+admin.site.register(Bookmarks)
+admin.site.register(Folders)
--- DjangoProject/bookmarksapp/models.py
+++ DjangoProject/bookmarksapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+
+from model_utils.models import TimeStampedModel
+
+from authenticationapp.models import MyUser
 
 # Create your models here.
+
+
+class Folders(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Folders, self).save(*args, **kwargs)
+
+
+class Bookmarks(TimeStampedModel):
+    folder = models.ForeignKey(Folders, on_delete=models.SET_NULL, related_name='folder', null=True)
+    name = models.CharField(max_length=50, unique=True)
+    url = models.URLField(max_length=200, unique=True)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, default=1)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Bookmarks, self).save(*args, **kwargs)
