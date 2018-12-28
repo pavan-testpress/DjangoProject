2ec2a935bbcc377a2f4c1223b2b5b7bf93baf0f8
pavan-testpress <45391567+pavan-testpress@users.noreply.github.com>
2018-12-28 10:45:28 +0530

    Add Authentication app (#3)
    ### Models
    - Add Custom MyUser Model
    ### Views
    - Add login 
    - Add logout
    - Add signup
    - Add index
    ### Templates
    - Add index
    - Add base
    - Add login
      - Add JQuery to fade invalid credentials
    - Add signup
    - Add invalidlogin
    ### Forms
    - Add UserCreation form
    ### Standards
    - Follow PEP8 import guidelines
    - Follow flake8 conventions
    - Add required packages to Pipfile for this app

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authenticationapp.apps.AuthenticationappConfig',
+    'bootstrap4',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'authenticationapp.MyUser'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
