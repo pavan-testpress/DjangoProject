246344f298bfaaca227b465a6f696ec68e6205b3
pavan-testpress <45391567+pavan-testpress@users.noreply.github.com>
2018-12-28 13:28:29 +0530

    Add Models to Bookmarks app (#4)
    * Add Models to Bookmarks app
    
    - Add Bookmarksapp to Installed apps
    - Add Bookmarks Model
    - Add Folders Model
    - Add django_utils Pipfile
    - Migrate the Models
    - Remove Unused imports to follow flake8 guidelines
    - Add Bookmarks, Folders Models to Admin site

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authenticationapp.apps.AuthenticationappConfig',
+    'bookmarksapp.apps.BookmarksappConfig',
     'bootstrap4',
     'crispy_forms'
 ]
