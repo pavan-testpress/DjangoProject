348d20627bd6756ad4c21eff60446e215e7e2a6b
Pavan Kumar <Pavan@testpress.in>
2018-12-31 17:41:17 +0530

    Add BookmarkList to Bookmarks app
    - Add BookmarkList template
    - Add BookmarkList View
    - Add Sort by name, bookmark created & modifide feature
    - Add Ubuntu fonts used in Css locally
    - Add FontAwesome icons locally

--- DjangoProject/bookmarksapp/views.py
+++ DjangoProject/bookmarksapp/views.py
@@ -17,8 +17,15 @@ class FolderListView(ListView):
     context_object_name = 'folders'
     template_name = 'bookmarksapp/folderlist.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super(FolderListView, self).dispatch(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
+
     def get_queryset(self):
         qs = super().get_queryset()
+        print(qs.filter(created_by=self.request.user))
         return qs.filter(created_by=self.request.user)
 
     def get_ordering(self):
@@ -28,5 +35,31 @@ class FolderListView(ListView):
                 sort = 'name'
                 return sort
             return sort
+        else:
+            return None
+
+
+class BookmarksListView(ListView):
+    model = Folders
+    context_object_name = 'bookmarks'
+    template_name = 'bookmarksapp/bookmarkslist.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super(BookmarksListView, self).dispatch(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
+
+    def get_queryset(self):
+        if 'sort' in self.request.GET:
+            self.queryset = Folders.objects.get(created_by=self.request.user, slug=self.kwargs['slug']).folder
+            qs = super(BookmarksListView, self).get_queryset()
+        else:
+            qs = Folders.objects.get(created_by=self.request.user, slug=self.kwargs['slug']).folder.all()
+        return qs
+
+    def get_ordering(self):
+        if 'sort' in self.request.GET:
+            return self.request.GET['sort']
         else:
             return None
