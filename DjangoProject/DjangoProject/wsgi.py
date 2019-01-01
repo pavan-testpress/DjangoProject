833df1205b1d04268f5c0e999d17eebc7daf356a
Pavan Kumar <Pavan@testpress.in>
2019-01-02 02:01:56 +0530

    Change wsgi

--- DjangoProject/DjangoProject/wsgi.py
+++ DjangoProject/DjangoProject/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
+sys.path.append('/home/ubuntu/Documents/DjangoProject/DjangoProject')
+sys.path.append('/home/ubuntu/Documents/DjangoProject/DjangoProject/DjangoProject')
 
 application = get_wsgi_application()
