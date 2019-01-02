75c6646da6a7e9d54a329354a7039de290eab0b7
Pavan Kumar <Pavan@testpress.in>
2019-01-02 11:37:55 +0530

    Add Login decorator - Followed flake8 guidelines - Changed testcases to use direct assert - Changed testcase name to test_create_duplicate_folder

--- DjangoProject/bookmarks/urls.py
+++ DjangoProject/bookmarks/urls.py
@@ -7,5 +7,10 @@ app_name = "bookmarks"
 urlpatterns = [
     path('', views.index, name="index"),
     path('folders/', views.FolderListView.as_view(), name="folders"),
+<<<<<<< HEAD:DjangoProject/bookmarks/urls.py
     path('folders/<slug>/', views.BookmarksListView.as_view(), name="folder-bookmarks")
+=======
+    path('folders/create/', views.FolderCreateView.as_view(), name="create_folder"),
+    path('folders/<slug>/', views.BookmarksListView.as_view(), name="bookmarks"),
+>>>>>>> edca14b... Add Login decorator:DjangoProject/bookmarksapp/urls.py
 ]
